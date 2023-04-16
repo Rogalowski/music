0f323267a3076c3217571bd7f347828b95acfce0
Rogalowski <rogalowski@gmail.com>
2023-04-16 17:19:30 +0200

    listing songs in playlist

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -1,13 +1,18 @@
 <script>
 // import useUserStore from "@/stores/user"
 import AppUpload from '@/components/Upload.vue'
+import CompositionItem from '@/components/CompositionItem.vue'
 import { songsCollection, auth } from '@/includes/firebase'
 export default { //beforeRoute Guard
   name: 'manage',
   components: {
-    AppUpload
+    AppUpload, CompositionItem
   },
-
+  data() {
+    return {
+      songs: []
+    }
+  },
   //guard for some components to get need auth, better way is use meta
   // beforeRouteEnter(to, from, next) {
   //   const store = useUserStore()
@@ -23,9 +28,17 @@ export default { //beforeRoute Guard
 //   this.$refs.upload.cancelUploads()
 //   next()
 // }
-async created() {
-  const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get()
-}
+  async created() { // get songs from database by collections filtered by currentuseruid
+    const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get()
+
+    snapshot.forEach(document => {
+      const song = {
+        ...document.data(),
+        docID: document.id,
+      }
+      this.songs.push(song)
+    });
+  }
 }
 </script>
 
@@ -50,53 +63,8 @@ async created() {
             </div>
             <div class="p-6">
               <!-- Composition Items -->
-              <div class="border border-gray-200 p-3 mb-4 rounded">
-                <div>
-                  <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                  <button
-                    class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
-                  >
-                    <i class="fa fa-times"></i>
-                  </button>
-                  <button
-                    class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
-                  >
-                    <i class="fa fa-pencil-alt"></i>
-                  </button>
-                </div>
-                <div>
-                  <form>
-                    <div class="mb-3">
-                      <label class="inline-block mb-2">Song Title</label>
-                      <input
-                        type="text"
-                        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                        placeholder="Enter Song Title"
-                      />
-                    </div>
-                    <div class="mb-3">
-                      <label class="inline-block mb-2">Genre</label>
-                      <input
-                        type="text"
-                        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                        placeholder="Enter Genre"
-                      />
-                    </div>
-                    <button
-                      type="submit"
-                      class="py-1.5 px-3 rounded text-white bg-green-600"
-                    >
-                      Submit
-                    </button>
-                    <button
-                      type="button"
-                      class="py-1.5 px-3 rounded text-white bg-gray-600"
-                    >
-                      Go Back
-                    </button>
-                  </form>
-                </div>
-              </div>
+              <composition-item v-for="song in songs" :key="song.docID" :song="song">
+              </composition-item>
             </div>
           </div>
         </div>
