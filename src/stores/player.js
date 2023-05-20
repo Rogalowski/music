45fc0f66c364c256df1e03a4c208b3799c792985
Rogalowski <rogalowski@gmail.com>
2023-05-20 17:56:27 +0200

    196. Playing Audio

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,13 +1,22 @@
 import { defineStore } from 'pinia'
-
+import { Howl} from 'howler'
 export default defineStore('player', {
     state: () => ({
         current_song: {},
+        sound: {},
     }),
     actions: {
         async newSong(song) {
             this.current_song = song
+            
+            this.sound = new Howl({
+                src: [song.url],
+                html5: true,  // will allow get songs from external api/database like firebase
+            })
+
+            this.sound.play()
         }
+    
     }
     
 })
