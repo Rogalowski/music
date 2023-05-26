03aeee89edcf2d64843d2a07009849dc332cb6cf
Rogalowski <rogalowski@gmail.com>
2023-05-26 14:34:47 +0200

    Translation pl

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,11 +1,13 @@
 import { createI18n } from 'vue-i18n'
 import en from "@/locales/en.json"
+import pl from "@/locales/pl.json"
 // https://www.science.co.il/language/Locale-codes.php
 
 export default createI18n({
     locale: "en",
     fallbackLocale: "en",
     messages: {
-        en
+        en,
+        pl
     },
 })
