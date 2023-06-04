efe07ff372d8dc2bd5ece0c129d001be2215f291
Rogalowski <rogalowski@gmail.com>
2023-06-04 17:53:21 +0200

    COmputed Watchers

--- MIXINS and COMPOSITION API/section-21-3-STARTER/src/App.vue
+++ MIXINS and COMPOSITION API/section-21-3-STARTER/src/App.vue
@@ -1,9 +1,42 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ num }} doubled: {{ double }}</p>
+    <button @click="incerement">+1 num</button>
+    <p>
+      <input type="text" v-model="phrase" />
+    </p>
+    {{ reversePhrase }}
+  </div>
 </template>
 
 <script>
+import { ref, watchEffect, toRefs, reactive, watch, computed } from 'vue'
 export default {
-  name: "App",
-};
+  name: 'App',
+  setup() {
+    let num = ref(0)
+    const phrase = ref('')
+    let reversePhrase = ref('')
+
+    function incerement() {
+      num.value++
+    }
+
+    const double = computed(() => {
+      return num.value * 2
+    })
+
+    watch([phrase], ([newVal, OldVal]) => {
+      reversePhrase.value = phrase.value.split('').reverse().join('')
+    })
+
+    return {
+      num,
+      incerement,
+      phrase,
+      reversePhrase,
+      double
+    }
+  }
+}
 </script>
