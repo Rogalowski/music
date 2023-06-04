b86007e5c69710d05299038c5e8d88d1abf7e326
Rogalowski <rogalowski@gmail.com>
2023-06-04 18:13:51 +0200

    Emmits

---
+++ MIXINS and COMPOSITION API/section-21-3-STARTER/src/Alert.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  props: ['user'],
+  setup(props, context) {
+    // context have emit
+    context.emit('eventName', {'payload'}) // the same as this.$emit
+  }
+}
+</script>
